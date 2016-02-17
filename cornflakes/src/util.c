3feb5d4d8363833dec4febbf1d6e24a86b3c9bb0
Alejandro Queiruga <afqueiruga@lbl.gov>
2016-02-17 13:57:39 -0800

    Untested VTK writer in C.

--- cornflakes/src/util.c
+++ cornflakes/src/util.c
@@ -1,7 +1,10 @@
 #include "util.h"
 
+#include <math.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
-#include "math.h"
 real_t dist(int dim, real_t * x, real_t * y) {
   int i;
   double ac = 0.0;
@@ -37,3 +40,58 @@ void Interpolate(real_t * uold, real_t * Xold, int Nold,
 
   SpatialHash_destroy(&sh);
 }
+
+
+
+void write_vtk(real_t * x, int gdim, int N, hypergraph_t * hg,
+	       char * names, real_t ** data, int Ndata,
+	       char * fname, ... )
+{
+  va_list args;
+  va_start( args, fname );
+  char buf[1024];
+  vsprintf(buf,fname,args);
+  FILE * fh = fopen(buf,"w");
+  if(fh==NULL) {
+    printf("ERROR: UNABLE TO OPEN OUTPUT FILE\n");
+    exit(-1);
+  }
+
+  int A,i;
+  
+  fprintf(fh, "# vtk DataFile Version 2.0\nGraph connectivity\nASCII\nDATASET UNSTRUCTURED_GRID\n");
+
+  // Write points
+  fprintf(fh,"POINTS %d doouble\n",N);
+  for(A=0; A<N; A++) {
+    if(gdim==2)
+      fprintf(fh,"%e %e 0\n",x[gdim*A+0],x[gdim*A+1]);
+    else
+      fprintf(fh,"%e %e %e\n",x[gdim*A+0],x[gdim*A+1],x[gdim*A+2]);
+  }
+
+  // Write edges
+  hyperedges_t * he = hg->he+0;
+  fprintf(fh,"\nCELLS %d %d\n",he->n_edge,he->n_edge*he->l_edge);
+  for(A=0;A<he->n_edge;A++) {
+    for(i=0;i<he->l_edge;i++) fprintf(fh,"%d ",he->edges[i]);
+    fprintf(fh,"\n");
+  }
+  fprintf(fh,"\nCELL_TYPES %d\n",he->n_edge);
+  int hetype = 0;
+  switch(he->n_edge) { // From the manual
+  case 1: hetype=1; break;
+  case 2: hetype=3; break;
+  case 3: hetype=5; break;
+  case 4: hetype=9; break;
+    //  case 4: hetype=10; break;
+  case 8: hetype=12; break;
+  default: hetype=1;
+  }
+  for(A=0;A<he->n_edge;A++) {
+    fprintf(fh,"%d\n",hetype);
+  }
+
+  // Write point data
+}
+
--- cornflakes/src/util.h
+++ cornflakes/src/util.h
@@ -2,11 +2,18 @@
 #define __UTIL_H
 
 #include "spatialhash.h"
+#include "hypergraph.h"
+
 
 real_t dist(int dim, real_t * x, real_t * y);
 
 void Interpolate(real_t * uold, real_t * Xold, int Nold,
 		 real_t * unew, real_t * Xnew, int Nnew,
 		 int udim, int xdim, real_t rad);
+
+
+void wrte_vtk(real_t * x, int gdim, int N, hypergraph_t * hg,
+	      char * names, real_t ** data, int Ndata,
+	      char * fname, ... );
 
 #endif
