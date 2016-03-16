41f3073f260aec5df101544c69f17ca64cdf23aa
Alejandro Queiruga <afqueiruga@lbl.gov>
2016-03-16 13:36:01 -0700

    Needed more array room in kernel.

--- cornflakes/src/kernel.h
+++ cornflakes/src/kernel.h
@@ -3,10 +3,10 @@
 
 typedef double real_t;
 
-#define KERNEL_MAP_MAX 5
-#define KERNEL_INP_MAX 5
-#define KERNEL_OUTP_MAX 5
-#define KERNEL_OUT_MAP_MAX 5
+#define KERNEL_MAP_MAX 10
+#define KERNEL_INP_MAX 10
+#define KERNEL_OUTP_MAX 10
+#define KERNEL_OUT_MAP_MAX 10
 // User only gets 31 characters to name
 #define KERNEL_NAME_MAX 32
 
