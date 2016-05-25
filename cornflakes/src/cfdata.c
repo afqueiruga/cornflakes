58abf611b12ce1636f5e42d8febabc13f462e296
Alejandro Queiruga <afqueiruga@lbl.gov>
2016-05-25 11:21:14 -0700

    CFData is now a generic Vector data type that wraps libraries

--- cornflakes/src/cfdata.c
+++ cornflakes/src/cfdata.c
@@ -1,23 +1,23 @@
 #include "cfdata.h"
 
-void CFData_Get(cfdata_t * self, int ndof,int * dofs, real_t * vals)
-{
+void CFData_Get_Values(cfdata_t * self, int ndof,int * dofs, real_t * vals) {
   self->vtable->Get_Values(self,ndof,dofs,vals);
 }
+void CFData_Place(cfdata_t * self, int n, int * dofs, real_t * vals) {
+  self->vtable->Place(self,n,dofs,vals);
+}
+void CFData_Wipe(cfdata_t * self) {
+  self->vtable->Wipe(self);
+}
+void CFData_Finalize(cfdata_t * self) {
+  self->vtable->Finalize(self);
+}
+void CFData_Destroy(cfdata_t * self) {
+  self->vtable->Destroy(self);
+}
 
 
-void CFData_Default_Get_Values(cfdata_t * self, int ndof,int *dofs, real_t * vals)
-{
-  int k;
-  for(k=0;k<ndof;k++) vals[k] = ((real_t*)self->data)[ dofs[k] ];
-}
-const _CFDATA_VTABLE_t cfdata_default_vtable = {
-  .Get_Values = CFData_Default_Get_Values
-};
-void CFData_Default_New(cfdata_t * self, real_t * payload) {
-  self->vtable = &cfdata_default_vtable;
-  self->data = payload;
-}
+
 
 #ifdef USE_PETSC
 
--- cornflakes/src/cfdata.h
+++ cornflakes/src/cfdata.h
@@ -5,14 +5,24 @@
 
 typedef struct _CFDATA_VTABLE_t _CFDATA_VTABLE_t;
 typedef struct cfdata_t {
+  int own;
+  int N;
   void * data;
   const _CFDATA_VTABLE_t * vtable;
 } cfdata_t;
 struct _CFDATA_VTABLE_t {
+  real_t * (*Place)(cfdata_t * self, int n, int * dofs, real_t * vals);
   void (*Get_Values)(cfdata_t * self, int ndof,int * dofs, real_t * vals);
+  void (*Wipe)(cfdata_t * self);
+  void (*Finalize)(cfdata_t * self);
+  void (*Destroy)(cfdata_t * self);
 };
 
-void CFData_Get(cfdata_t * self, int ndof,int * dofs, real_t * vals);
+void CFData_Get_Values(cfdata_t * self, int ndof,int * dofs, real_t * vals);
+void CFData_Place(cfdata_t * self, int n, int * dofs, real_t * vals);
+void CFData_Wipe(cfdata_t * self);
+void CFData_Finalize(cfdata_t * self);
+
 void CFData_Default_New(cfdata_t * self, real_t * payload);
 
 #ifdef USE_PETSC
