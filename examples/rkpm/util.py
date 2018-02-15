503d665c1387cfda0a909e4a8f06568f3306dad0
Alejandro Queiruga <afqueiruga@lbl.gov>
2018-02-14 18:07:31 -0800

    Plotting out all of them

--- examples/rkpm/util.py
+++ examples/rkpm/util.py
@@ -1,4 +1,8 @@
 import numpy as np
+
+#
+# Generation a Quadrature background mesh
+#
 Quad_Tables = {
     1: [ (2.0, 0.0) ],
     2: [ (1.0,-np.sqrt(1.0/3.0)),(1.0,np.sqrt(1.0/3.0) ) ],
@@ -33,23 +37,62 @@ def GaussQuadGrid(Nrx,Nry, Nptx, Npty, start, L,H):
                 w[j*Nrx*Npty*Nptx + Nptx*Npty*i+k]  = wz * l*h/4.0
     return w,x
 
+#
+# Evaluating the RKPM method
+#
+def eval_rkpm(X,u, y, grad=False):
+    #H,r = cf.Graphers.Build_Proximity_Graph_Given_Length(X,Ndesired, cutoff , y)
+    r = SupR*np.ones(y.shape[0])
+    H = cf.Graphers.Build_Proximity_Graph_Variable(X,r, y)
+    #H = cf.Graphers.Build_Proximity_Graph_Uniform(X,SupR, y)
+    if grad:
+        ke = hr.kernel_interp_grad_u
+    else:
+        ke = hr.kernel_interp_u
+    ui, = cf.Assemble(ke, H,
+                      {'x':(X,dm_ptvec),
+                       'sup':(r,dm_ptsca),
+                       'u':(u,dm_ptsca),
+                       'y':(y,dm_ptvec)},
+                      {'uy':(dm_ptsca,)},
+                      ndof=y.shape[0])
+    return ui
+def eval_shape(X, i, sample=100, grad=False):
+    a = np.zeros(X.shape[0])
+    a[i] = 1.0
+    xprobe = cf.PP.init_grid(sample,sample,[-L,-L],[2*L,0],[0,2*L])
+    shapei = eval_rkpm(X,a, xprobe, grad)
+    return xprobe,shapei
+def eval_rkpm_grid(X, ui, sample=100, grad=False):
+    xprobe = cf.PP.init_grid(sample,sample,[-L,-L],[2*L,0],[0,2*L])
+    u = eval_rkpm(X,ui, xprobe, grad)
+    return xprobe,u
 
-def surfplot(Z, u):
-    import scipy.spatial
-    from matplotlib import pylab as plt
-    from mpl_toolkits.mplot3d import Axes3D
-    tri = scipy.spatial.Delaunay(Z)
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1, projection='3d')
-    ax.plot_trisurf(Z[:,0], Z[:,1], u, triangles=tri.simplices, cmap=plt.cm.Spectral,linewidth=0, antialiased=False)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    plt.show()
+#
+# Output routines
+#
 def write_cloud(fname, X, nodefields):
     import cornflakes as cf
     Hcloud = cf.Hypergraph()
     for l in xrange(X.shape[0]): Hcloud.Push_Edge([l])
     cf.GraphIO.write_graph(fname, Hcloud, X,nodefields)
+
+def surfplot(Z, u, ax=None):
+    import scipy.spatial
+    from matplotlib import pylab as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    tri = scipy.spatial.Delaunay(Z)
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1, projection='3d')
+        show=True
+    else:
+        show=False
+    ax.plot_trisurf(Z[:,0], Z[:,1], u, triangles=tri.simplices, cmap=plt.cm.Spectral,linewidth=0, antialiased=False)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    if show:
+        plt.show()
 
 def surfplotly(Z, u):
     import scipy.spatial
